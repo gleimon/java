f16c70e7b3fd33a5e271e83657ca84cec9890bd5
gleimon <93679771+gleimon@users.noreply.github.com>
2022-04-07 16:34:32 -0300

    Update tabuada.java

--- calculo_reservatorio/tabuada/tabuada.java
+++ calculo_reservatorio/tabuada/tabuada.java
@@ -3,8 +3,8 @@ package tabuada;
 public class tabuada {
         public static void main(String[] args) {
             for(int multiplicacao = 1; multiplicacao <10; multiplicacao++){
-            for(int contadory = 1; contadory <=10; contadory++){
-            System.out.print(multiplicacao * contadory);
+            for(int contador = 1; contador <=10; contador++){
+            System.out.print(multiplicacao * contador);
             System.out.print(" ");
             }
             System.out.println();
